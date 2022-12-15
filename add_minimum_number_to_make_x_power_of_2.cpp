6bd1376a428dde93bc8c8aaeb41e72b7baf283a2
Bibek Das <101031570+bibek-das@users.noreply.github.com>
2022-12-16 02:19:34 +0600

    Update add_minimum_number_to_make_x_power_of_2.cpp

--- add_minimum_number_to_make_x_power_of_2.cpp
+++ add_minimum_number_to_make_x_power_of_2.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool is_pow_two(long long x) {
+bool ispow2(long long x) {
     if (x == 0) return false;
     return (ceil(log2(x)) == floor(log2(x)));
 }
 
 long long add_x(long long x) {
-    if (is_pow_two(x)) return 0;
+    if (ispow2(x)) return 0;
     // minimum number to add in x to make x power of 2
     return (1 << (__lg(x) + 1)) - x;
 }
